4b23e5acca8556691af2c4d3bef9e12fbf1aaa45
Adimachukwu Benjamin <thegreatben27@gmail.com>
2024-01-29 14:26:30 +0100

    buit the new issue page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import { Button } from '@radix-ui/themes'
+import { Button, TextArea } from '@radix-ui/themes'
+import Link from 'next/link'
 
 const IssuesPage = () => {
   return (
     <div>
       <Button>
-        new Issue
+       <Link href='/issues/new'>New Issue</Link>
       </Button>
+      
       </div>
   )
 }
