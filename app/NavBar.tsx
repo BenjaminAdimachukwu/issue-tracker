fc7987ba9aa0300140a424a05d346fc155a7a53a
Adimachukwu Benjamin <thegreatben27@gmail.com>
2024-02-04 17:05:30 +0100

    Change the layout of the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,7 +5,7 @@ import { FaBug } from "react-icons/fa";
 import { usePathname } from 'next/navigation'
 import classNames from 'classnames';
 import { useSession } from 'next-auth/react';
-import { Box } from '@radix-ui/themes';
+import { Box, Container, Flex } from '@radix-ui/themes';
 
 const NavBar = () => {
     const currentPath = usePathname()
@@ -15,13 +15,17 @@ const NavBar = () => {
     {label: 'Issues', href: '/issues/list'},
 ]
   return (
-    <nav className='flex space-x-6 border-b  px-5 items-center h-14'> 
-        <Link href='/'><FaBug /></Link>
+    <nav className=' border-b  py-3 px-5'> 
+    <Container>
+
+    <Flex justify='between'>
+    <Flex gap='3' align='center'>
+    <Link href='/'><FaBug /></Link>
         <ul className='flex space-x-6'>
            {links.map(link =>
            <li key={link.href}>
              <Link  className={classNames({
-              'text-zinc-900': link.href === currentPath,
+               'text-zinc-900': link.href === currentPath,
               'text-zinc-500' : link.href != currentPath,
               'hover:text-zinc-800 transition-colors' : true
             })} href={link.href}>
@@ -31,11 +35,15 @@ const NavBar = () => {
            )}
            
         </ul>
-        <Box>
+    </Flex>
+    <Box>
+    <Box>
           {status === 'authenticated' && (<Link href='/api/auth/signout'>log out</Link>)}
           {status === 'unauthenticated' && (<Link href='/api/auth/signin'>log in</Link>)}
         </Box>
-
+    </Box>
+    </Flex> 
+              </Container>
     </nav>
   )
 }
